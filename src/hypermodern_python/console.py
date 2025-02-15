4a4e5303a514f5041bd819cea04877c9028a792c
BrunoMenegozzo <brunomenegozzo@mi.unc.edu.ar>
2025-02-15 13:12:08 -0300

    Estudio de Poetry, ejemplo con API de wikipedia

---
+++ src/hypermodern_python/console.py
@@ -0,0 +1,25 @@
+import click
+import textwrap
+import requests
+
+
+from . import __version__
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+    if not data:
+        click.echo("Opss! Something is wrong. Wikipedia dosen't want to talk")
+        return
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
